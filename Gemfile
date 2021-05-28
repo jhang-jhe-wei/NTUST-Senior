321872b114ab326736766b7514841fe6321f3069
wells <jhang0912407249@gmail.com>
2021-05-28 20:18:38 +0800

    Add capistrano gem

--- Gemfile
+++ Gemfile
@@ -26,6 +26,10 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.4', require: false
 
 group :development, :test do
+  gem "capistrano-rails"
+  gem "capistrano-passenger"
+  gem 'capistrano-rvm'
+  gem 'capistrano-bundler'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module NTUSTSenior
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.hosts << "chatbot.wells.tw"
+    config.hosts << "test.wells.tw"
+    config.hosts << "ntust-senior-chatbot.wells.tw"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
