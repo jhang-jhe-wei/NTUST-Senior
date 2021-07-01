1a31e1f34a99d9bae29617b8067593230c8a8c92
wells <jhang0912407249@gmail.com>
2021-07-01 14:25:30 +0800

    Add junior school community

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get "新生引導", to: "guides#junior_index"
   #新生該做的事
   get '設定信箱轉寄', to: "junior#mail_foward"
+  get '加入台科大相關社群',to: 'introductions#school_community'
   #課務專區
   get '課務',to: 'courses#class_event'
   get '選課規定',to: 'courses#course_rule'
