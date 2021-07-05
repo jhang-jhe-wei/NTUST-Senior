616ca0005f6362ef6fa8ade493bf6f6d053447c6
wells <jhang0912407249@gmail.com>
2021-07-05 17:31:54 +0800

    Add image_flex

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def image_flex(*image_urls, **option)
+    image_urls = image_urls.flatten
+    Kamiflex.hash(self) do
+      carousel do
+        bubbles image_urls, size: :giga do |image_url|
+          body paddingAll: "0px" do
+            image image_url, {size: :full, aspectRatio: "1764:1106", aspectMode: :fit, action: uri_action(image_url)}.merge(option)
+          end
+        end
+      end
+    end
+  end
 end
