ea246b852dc43a1ef22fee61c80ad308669ba176
wells <jhang0912407249@gmail.com>
2021-08-03 15:02:19 +0800

    Update github webhook event

--- app/controllers/system_events_controller.rb
+++ app/controllers/system_events_controller.rb
@@ -3,10 +3,10 @@ class SystemEventsController < ApplicationController
     def github
         if params.dig(:system_event, :action) == "published"
             version = params.dig(:release, :tag_name)
-            body = params.dig(:release, :body)
+            body = params.dig(:release, :body).gsub!('#', '📋').gsub!('-', '📌')
             subscriptions = Subscription.where("notify_type = ?", "版本更新通知")
             subscriptions.each do |subscription|
-                LineNotify.send(subscription.user.line_notify_token, message: "\n\n版本更新通知\n\n版本號：#{version}\n\n#{body}")
+              LineNotify.send(subscription.user.line_notify_token, message: "\n\n版本更新通知\n\n💎 最新版本號：#{version}\n\n本次更新內容為以下：\n\n#{body}")
             end
         end
         render status: 200, json: {status: "OK"}
