51d140ee516f202942d5c4e913e39f7504329113
wells <jhang0912407249@gmail.com>
2022-12-08 23:30:36 +0100

    feat: make continue chatting

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -4,14 +4,16 @@ class GuidesController < ApplicationController
   def share_bot; end
 
   def other
+    last_response = Rails.cache.read("last-reponse-#{current_user.line_id}") || ''
     response = OpenAIClient.completions(
       parameters: {
         model: 'text-davinci-003',
-        prompt: params[:other],
+        prompt: last_response + params[:other],
         max_tokens: 1000
       }
     )
 
     @reply_text = response['choices'].map { |c| c['text'] }.join('').strip
+    Rails.cache.write("last-reponse-#{current_user.line_id}", @reply_text, expires_in: 30.minutes)
   end
 end
