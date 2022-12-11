ac36951f0ec98ae5e9e330d9e7995332279227b8
wells <jhang0912407249@gmail.com>
2022-12-11 18:45:43 +0100

    feat: feat: remember recently up to 20 conversation records

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class GuidesController < ApplicationController
+  after_action :cache_conversation_records, only: :other
+
   def share_bot; end
 
   def reset_chatting
@@ -8,16 +10,35 @@ class GuidesController < ApplicationController
   end
 
   def other
-    last_response = Rails.cache.read("last-reponse-#{current_user.line_id}") || ''
+    append_conversation_record(params[:other])
     response = OpenAIClient.completions(
       parameters: {
         model: 'text-davinci-003',
-        prompt: last_response + params[:other],
+        prompt: conversation_records.join,
         max_tokens: 1000
       }
     )
 
     @reply_text = response['choices'].map { |c| c['text'] }.join('').strip
-    Rails.cache.write("last-reponse-#{current_user.line_id}", @reply_text, expires_in: 30.minutes)
+    append_conversation_record(@reply_text)
+  end
+
+  private
+
+  def cache_conversation_records
+    Rails.cache.write(
+      "chatting-histories-#{current_user.line_id}",
+      conversation_records,
+      expires_in: 30.minutes
+    )
+  end
+
+  def conversation_records
+    @conversation_records ||= Rails.cache.read("chatting-histories-#{current_user.line_id}") || []
+  end
+
+  def append_conversation_record(str)
+    conversation_records.shift if conversation_records.size >= 20
+    conversation_records << str
   end
 end
