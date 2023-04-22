d8fe869c094ad039a068d5fb5ac72cb736fa34fc
wells <jhang0912407249@gmail.com>
2023-04-22 16:47:36 +0800

    chore: add path to firefox and geckodriver

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -2,9 +2,11 @@
 
 Capybara.register_driver :firefox_headless do |app|
   options = Selenium::WebDriver::Firefox::Options.new
+  options.binary = ENV['FIREFOX_BIN']
   options.args << '--headless' # enable headless mode
   Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
 end
 
+Selenium::WebDriver::Firefox::Service.driver_path = ENV['GECKODRIVER_PATH']
 Capybara.default_driver = :firefox_headless
 Capybara.default_max_wait_time = 1
